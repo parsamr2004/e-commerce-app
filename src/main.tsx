f5d4b44b95f43370cb333f7e4e1adef0e8e982d3
Saman <saman.1384.alishahi@gmail.com>
2025-10-05 21:59:22 +0330

    feat: :ambulance:
    #1 App added to main Mr Fallah

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import LoginPage from "./pages/authentication/LoginPage.tsx";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <LoginPage />
+    <App />
   </StrictMode>
 );
