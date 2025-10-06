957c2efb20ec9b59e410d186c86f36e44b94db6e
Parsamr2004 <mr.parsa.dehini.2004@gmail.com>
2025-10-06 12:17:45 -0700

    feat: :lipstick: #11 first change for CartPage task

---
+++ src/components/ProductCardItem.tsx
@@ -0,0 +1,58 @@
+import { Card } from "@/components/ui/card";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectContent,
+  SelectValue,
+} from "@/components/ui/select";
+import { LucideTrash2 } from "lucide-react";
+
+interface ProductCardItemprops {
+  version: string;
+  name: string;
+  price: string;
+  url: string;
+}
+
+const ProductCardItem = (props: ProductCardItemprops) => {
+  const { version, name, price, url } = props;
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <div className="w-[88px] h-[88px]">
+          <img src={url} alt="" />
+        </div>
+        <div className="flex flex-col gap-1">
+          <span className="text-primary">{version}</span>
+          <span>{name}</span>
+          <span className="font-bold">{price} تومان</span>
+        </div>
+      </div>
+      <div>
+        <div className="flex gap-2 justify-end">
+          <Select>
+            <SelectTrigger className="w-[96px] border-input bg-white shadow-none cursor-pointer">
+              <SelectValue placeholder="1" />
+            </SelectTrigger>
+            <SelectContent
+              side="right"
+              className="cursor-pointer w-[var(--radix-select-trigger-width)]"
+            >
+              <SelectItem value="1">1</SelectItem>
+              <SelectItem value="2">2</SelectItem>
+              <SelectItem value="3">3</SelectItem>
+            </SelectContent>
+          </Select>
+          <Button className="cursor-pointer" size="icon" variant="ghost">
+            <LucideTrash2 className="text-destructive" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCardItem;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import CartPage from "./pages/user/CartPage";
+// import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <CartPage />
   </StrictMode>
 );
