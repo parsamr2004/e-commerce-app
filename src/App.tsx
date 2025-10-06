79c8e950c757423d09e15d4a599235e0f2d82588
Saman <saman.1384.alishahi@gmail.com>
2025-10-06 21:37:33 +0330

    feat: :lipstick:
    #4 Structur added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 // import { Badge } from "./components/ui/badge";
+import { ThemeProvider } from "./components/ui/darkmode/theme-provider";
 import Layout from "./Layout";
 
 const App = () => {
   return (
-    <Layout>
-      <div>
-        <h1>صفحه اصلی</h1>
-      </div>
-    </Layout>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Layout>
+        <div>
+          <h1>صفحه اصلی</h1>
+        </div>
+      </Layout>
+    </ThemeProvider>
   );
 };
 
