5e0a30e63d2fd79fa6e0862deb6d3417eb8eaf71
saeedeMasoudinejad <58415361+saeedeMasoudinejad@users.noreply.github.com>
2025-10-10 23:32:17 +0330

    Define Shop page

--- src/pages/user/ShopPage.tsx
+++ src/pages/user/ShopPage.tsx
@@ -25,21 +25,22 @@ const RadioItem = (props:RadioItemType) =>{
 const ShopPage = () => {
   return (
     <Layout>
-      <div className="border flex">
+      <div className="flex">
          {/* bg-base-side */}
-        <div className="border max-w-[264px] w-full bg-base-side">
-          <RadioGroup className="border my-7">
+        <div className="max-w-[264px] w-full bg-base-side">
+          <RadioGroup className="my-7 mx-3">
             <p className="bg-card text-foreground rounded-2xl py-1 px-10 text-center text-[14px]">فیلتر برند</p>
             <RadioItem valueItem="option_one" textItem="Option One"></RadioItem>
             <RadioItem valueItem="option_two" textItem="Option Two"></RadioItem>
           </RadioGroup>
-          <div className="flex flex-col items-center">
-            <label className="bg-card text-foreground py-2 px-10 rounded-2xl w-full text-center text-[14px]">فیلتر قیمت</label>
-            <input type="text" className="rounded-xl px-2.5 py-2 placeholder:text-[10px] placeholder:font-ligh bg-white text-secoundry m-5" placeholder="قیمت را وارد نمایید"/>
+          <div className="flex flex-col items-center mx-3">
+            <label className="bg-card text-foreground py-2 px-10 rounded-2xl w-full text-center text-[14px] bg-color">فیلتر قیمت</label>
+            <input type="text" className="rounded-xl px-2.5 py-2 placeholder:text-[10px] placeholder:font-ligh bg-card text-secoundry m-5" placeholder="قیمت را وارد نمایید"/>
           </div>
-          <Button size="lg"  className="mt-4 border w-full bg-card text-foreground">حذف فیلتر</Button>
+          {/* <button className="mt-4 border border-gray-400   rounded-sm mx-auto block px-14 ">حذف فیلتر</button> */}
+          <Button size="lg" className="mt-4 border border-gray-400 bg-base-side text-foreground rounded-sm mx-auto block px-14">حذف فیلتر</Button>
         </div>
-        <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-4 mr-16 mb-[92px]">
+        <div className="grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3 gap-4 mr-16 mb-[92px] mx-8">
           <ShopCard></ShopCard>
           <ShopCard></ShopCard>
           <ShopCard></ShopCard>
