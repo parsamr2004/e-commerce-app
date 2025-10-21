3344f2be60666513ca6f03245c9708797d7831af
sohahe <soha.he74@gmail.com>
2025-10-21 17:31:22 +0330

    ActivateStepperandaddrouteforshoppingprogress

---
+++ src/pages/user/ShoppingProgress.tsx
@@ -0,0 +1,20 @@
+import Stepper from "@/components/Stepper";
+import { useLocation, Outlet } from "react-router-dom";
+
+const ShoppingProgress = () => {
+  const location = useLocation();
+
+  let currentStep = 2;
+  if (location.pathname.endsWith("/summary")) {
+    currentStep = 3;
+  }
+
+  return (
+    <div className="mx-auto max-w-5xl px-4 py-8">
+      <Stepper currentStep={currentStep} />
+      <Outlet />
+    </div>
+  );
+};
+
+export default ShoppingProgress;
