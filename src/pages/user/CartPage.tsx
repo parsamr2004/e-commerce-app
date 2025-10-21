0e87de827496f03c4b6e3d8508bd5d2d3ebc98f6
Parsamr2004 <mr.parsa.dehini.2004@gmail.com>
2025-10-21 08:00:00 -0700

    feat: Navigate Button to Shopping-progress page

--- src/pages/user/CartPage.tsx
+++ src/pages/user/CartPage.tsx
@@ -1,33 +1,40 @@
 import ProductCardItem from "@/components/ProductCardItem";
 import { Button } from "@/components/ui/button";
 import useCartStore from "@/stores/use-cart-store";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 const CartPage = () => {
   const { cartItems } = useCartStore();
-  const counter =  cartItems.reduce((acc, cur) => acc + (cur.countInBasket || 0), 0);
-  const finalAmout = cartItems.reduce((acc, cur) => acc + (cur.countInBasket || 0) * (cur.price || 0), 0);
+  const navigate = useNavigate();
+  const counter = cartItems.reduce((acc, cur) => acc + (cur.countInBasket || 0), 0);
+  const finalAmout = cartItems.reduce(
+    (acc, cur) => acc + (cur.countInBasket || 0) * (cur.price || 0),
+    0
+  );
 
-  if (cartItems.length === 0) return <div className="p-5">محصولی برای خرید وجود ندارد.</div>
+  if (cartItems.length === 0) return <div className="p-5">محصولی برای خرید وجود ندارد.</div>;
 
   return (
     <div className="flex flex-col gap-8 px-10 py-5">
       <div className="flex flex-col gap-5">
         {cartItems.map((cartItem) => (
-          <ProductCardItem
-            cartItem = { cartItem }
-          />
+          <ProductCardItem cartItem={cartItem} />
         ))}
       </div>
 
       <div className="flex flex-col gap-2">
         <p className="text-xl">
-          تعداد <span>{counter === 0? cartItems.length: counter}</span>
+          تعداد <span>{counter === 0 ? cartItems.length : counter}</span>
         </p>
         <span className="text-xl font-bold">{finalAmout.toLocaleString()} تومان</span>
-        <Link to="">
-          <Button className="cursor-pointer self-start rounded-2xl px-50">تکمیل خرید</Button>
-        </Link>
+        <Button
+          className="cursor-pointer self-start rounded-2xl px-50"
+          onClick={() => {
+            navigate("/shopping-progress");
+          }}
+        >
+          تکمیل خرید
+        </Button>
       </div>
     </div>
   );
